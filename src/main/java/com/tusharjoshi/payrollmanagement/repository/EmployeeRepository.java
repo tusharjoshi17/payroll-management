460d144c628f6c6cb98d5f2b3b623e6e13bdfa87
Tushar Joshi <tusharjoshi52@gmail.com>
2023-10-24 01:15:31 +0530

    wrote crud methods in JpaRepository which will be shifted in service methods now in service file

--- src/main/java/com/tusharjoshi/payrollmanagement/repository/EmployeeRepository.java
+++ src/main/java/com/tusharjoshi/payrollmanagement/repository/EmployeeRepository.java
@@ -1,13 +1,28 @@
 package com.tusharjoshi.payrollmanagement.repository;
 
 import com.tusharjoshi.payrollmanagement.model.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
-public interface EmployeeRepository extends BaseRepository<Employee, Long> {
-    public class EmployeeService {
-        private final EmployeeRepository employeeRepository;
-
-        public EmployeeService(EmployeeRepository employeeRepository) {
-            this.employeeRepository = employeeRepository;
-        }
-    }
+public interface EmployeeRepository extends JpaRepository<Employee,Long>{
+//    @Service
+//    public class EmployeeService{
+//        private final EmployeeRepository employeeRepository;
+//
+//        public EmployeeService(EmployeeRepository employeeRepository) {
+//            this.employeeRepository = employeeRepository;
+//        }
+//
+//        public Employee save(Employee employee){
+//            return employeeRepository.save(employee);
+//        }
+//
+//        public Employee findById(Long id){
+//            return employeeRepository.findById(id).orElseThrow(()->new RuntimeException("Employee not found with id: "+id));
+//        }
+//
+//        public void deleteById(Long id){
+//            employeeRepository.deleteById(id);
+//        }
+//    }
 }
