eaf0d32be1ac3527c23446372400ee7936475a32
Tushar Joshi <tusharjoshi52@gmail.com>
2023-11-24 20:01:35 +0530

    JPA/Hibernate database part done (3)
    Changes made-
    Added more dependencies called Hibernate validator for @NotNull, Future, Past, PresentOrFuture

--- src/main/java/com/tusharjoshi/payrollmanagement/repository/WorkingHistoryRepository.java
+++ src/main/java/com/tusharjoshi/payrollmanagement/repository/WorkingHistoryRepository.java
@@ -3,6 +3,9 @@ package com.tusharjoshi.payrollmanagement.repository;
 import com.tusharjoshi.payrollmanagement.model.entities.WorkingHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository for performing CRUD operations on the SalaryInformation entity.
+ */
 public interface WorkingHistoryRepository extends JpaRepository<WorkingHistory,Long> {
 
 }
